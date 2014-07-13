cd228f8ae06dd92fd4952535845c7fac60a38bc2
Carnes <whoismaha@gmail.com>
2014-07-13 16:09:25 -0400

    Refactored & reordered character class

--- php/main/character.php
+++ php/main/character.php
@@ -3,6 +3,7 @@ class character
 {
     private $alignment;
     private $hitPoints;
+    private static $abilityModifierLookup = array(null,-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5);
 
     public $name;
     public $armorClass;
@@ -28,30 +29,41 @@ class character
         $this->hitPoints = $this->getMaxHitPoints();
     }
 
-    private function isAlive(){
-        if($this->hitPoints>0)
-            return true;
-        return false;
+    public function __get($property){
+        if($property=="isAlive")
+            return $this->isAlive();
+        if($property=="strengthModifier")
+            return $this->abilityModifier($this->strength);
+        if($property=="dexterityModifier")
+            return $this->abilityModifier($this->dexterity);
+        if($property=="hitPoints")
+            return $this->hitPoints;
+        if($property=="level")
+            return $this->getLevel();
+        if($property=="maxHitPoints")
+            return $this->getMaxHitPoints();
+        return $this->$property;
     }
 
-    private static $abilityModifierLookup = array(null,-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5);
-    private function abilityModifier($stat)
-    {
-        return self::$abilityModifierLookup[$stat];
+    public function __set($property, $value){
+        if($property=="alignment")
+            if($value == alignment::Good || $value == alignment::Neutral || $value == alignment::Evil)
+                $this->alignment = $value;
     }
 
-    private function hitPointsPlusConstitution()
+    public function takeDamage($amount)
     {
-        $bonus = $this->abilityModifier($this->constitution);
-        if($bonus < 0)
-            $bonus = 0;
-        return $bonus + $this->hitPoints;
+        $this->hitPoints-=$amount;
     }
 
-    private function getLevel()
+    public function getAttackDamage($attackRole)
     {
-        $xpLevel = floor($this->experience / 1000);
-        return $xpLevel + 1;
+        $damage = $this->getAttackDamagePerLevel() + $this->strengthModifier;
+        if($attackRole==20)
+            $damage *= 2;
+        if($damage<1)
+            $damage=1;
+        return $damage;
     }
 
     public function addClass($classType)
@@ -60,6 +72,24 @@ class character
         if(in_array("characterClass",$interfaces))
             $this->class[] = new $classType();
     }
+
+    private function isAlive(){
+        if($this->hitPoints>0)
+            return true;
+        return false;
+    }
+
+    private function abilityModifier($stat)
+    {
+        return self::$abilityModifierLookup[$stat];
+    }
+
+    private function getLevel()
+    {
+        $xpLevel = floor($this->experience / 1000);
+        return $xpLevel + 1;
+    }
+
 
     private function getModifiers()
     {
@@ -90,46 +120,8 @@ class character
         return $bestResult;
     }
 
-    public function getAttackDamage($attackRole)
-    {
-        $damage = $this->getAttackDamagePerLevel() + $this->strengthModifier;
-        if($attackRole==20)
-            $damage *= 2;
-        if($damage<1)
-            $damage=1;
-        return $damage;
-    }
-
-    public function __get($property){
-        if($property=="isAlive")
-            return $this->isAlive();
-        if($property=="strengthModifier")
-            return $this->abilityModifier($this->strength);
-        if($property=="dexterityModifier")
-            return $this->abilityModifier($this->dexterity);
-        if($property=="hitPoints")
-            //return $this->hitPointsPlusConstitution();
-            return $this->hitPoints;
-        if($property=="level")
-            return $this->getLevel();
-        if($property=="maxHitPoints")
-            return $this->getMaxHitPoints();
-        return $this->$property;
-    }
-
     private function getMaxHitPoints()
     {
         return (5 * $this->getLevel()) + $this->abilityModifier($this->constitution);
-    }
-
-    public function __set($property, $value){
-        if($property=="alignment")
-            if($value == alignment::Good || $value == alignment::Neutral || $value == alignment::Evil)
-                $this->alignment = $value;
-    }
-
-    public function takeDamage($amount)
-    {
-        $this->hitPoints-=$amount;
     }
 }
